d56d0fc7df835791fee9fc08af8c6fda72df2cbe
cr4fun <cr4fun@outlook.com>
2019-05-13 09:59:48 +0800

    test

---
+++ src/views/About.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="about">
+    <h1>这是一个SDAG区块链生成器</h1>
+    <p>方便用户配置自己的SDAG区块链</p>
+    <p>用户填写app名称、logo、token总量、token符号、小数位，即可生成配置文件和一键安装脚本。</p>
+  </div>
+</template>
---
+++ src/views/Home.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" width="80" src="../assets/logo.png">
+    <el-form :model="blockchain" label-width="80px">
+      <el-form-item label="区块链名称" label-width="100">
+        <el-input v-model="blockchain.name"></el-input>
+      </el-form-item>
+      <el-form-item label="区块链logo" label-width="100">
+        <el-input v-model="blockchain.logo"></el-input>
+      </el-form-item>
+      <el-form-item label="Token发行总量" label-width="100">
+        <el-input v-model="blockchain.total"></el-input>
+      </el-form-item>
+      <el-form-item label="基金会地址" label-width="100">
+        <el-input v-model="blockchain.address"></el-input>
+      </el-form-item>
+      <el-button type="primary" plain>builder</el-button>
+    </el-form>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
+
+export default {
+  name: "home",
+  data(){
+    return {
+      blockchain:{}
+    }
+  },
+  components: {
+    HelloWorld
+  }
+};
+</script>
