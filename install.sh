f95d9a502c92d003204cf503b8b3503bef296317
Hui Min HE <joyioushe@gmail.com>
2019-05-16 19:22:30 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,2 @@
 docker pull sdag/sdag_testnet_dev
-docker cp sdag_testnet_dev: ./config.json ~/sdag
-docker run --rm -d --name sdag -p 6615:6615 -p 8080:8080 sdag/sdag_testnet_dev
+docker run --rm -d --name sdag -p 6615:6615 -p 8080:8080 -v $PWD/config.json:/sdag/explorer/static/config.json sdag/sdag_testnet_dev
